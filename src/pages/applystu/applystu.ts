245402302fc130fbd98175e7d9153f1b84a8c0df
liye45 <1412154147@qq.com>
2020-05-07 10:19:57 +0800

    新增3页

---
+++ src/pages/applystu/applystu.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core';
+import {  NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-applystu',
+  templateUrl: 'applystu.html'
+})
+export class ApplystuPage {
+  
+  constructor(public navCtrl: NavController) {
+  }
+  // setBackButtonText(){
+  //   this.navCtrl.pop();
+  // }
+  
+}
---
+++ src/pages/applyteach/applyteach.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core';
+import {  NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-applystu',
+  templateUrl: 'applyteach.html'
+})
+export class ApplyteachPage {
+  
+  constructor(public navCtrl: NavController) {
+  }
+  // setBackButtonText(){
+  //   this.navCtrl.pop();
+  // }
+  
+}
